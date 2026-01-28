33322bf2eef655d6abb5f46d3c0344a6c8754f2b
IgnacioBlanco <nanoblaco@gmail.com>
2026-01-28 11:19:05 -0600

    Semana 1 y 2

---
+++ Tienda_IgnacioB/src/main/java/Tienda_IgnacioB/demo/TiendaIgnacioBApplication.java
@@ -0,0 +1,13 @@
+package Tienda_IgnacioB.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIgnacioBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIgnacioBApplication.class, args);
+	}
+
+}
