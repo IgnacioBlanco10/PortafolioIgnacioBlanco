414867d346fcf277b0e27edc7d9e38e6633b0387
IgnacioBlanco <nanoblaco@gmail.com>
2026-01-29 15:15:23 -0600

    Semana 3 (código semana2)
    Se sube el código que teníamos que trabajar según las diapositivas de semana 2, pero las hicimos en semana 3 porque vamos una semana atras

--- Tienda_IgnacioB/src/main/java/Tienda_IgnacioB/demo/TiendaIgnacioBApplication.java
+++ Tienda_IgnacioB/src/main/java/Tienda_IgnacioB/demo/TiendaIgnacioBApplication.java
@@ -2,8 +2,10 @@ package Tienda_IgnacioB.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication // <--- CORREGIDO: Solo se usa la anotación base
+@ComponentScan (basePackages = {"Tienda_IgnacioB", "com.tienda"}) // cambios
 public class TiendaIgnacioBApplication {
 
 	public static void main(String[] args) {
